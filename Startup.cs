7ada5b90fa2867b7bb8c7b2b56c5cfa649c347bd
dolidimit <104081589+dolidimit@users.noreply.github.com>
2022-04-23 17:46:26 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,86 @@
+using DigitalCardsAppll.Data;
+using DigitalCardsAppll.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalCardsAppll
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DigitalCardsDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            services
+               .AddDefaultIdentity<IdentityUser>(options =>
+               {
+                   options.Password.RequireDigit = false;
+                   options.Password.RequireLowercase = false;
+                   options.Password.RequireNonAlphanumeric = false;
+                   options.Password.RequireUppercase = false;
+               })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<DigitalCardsDbContext>();
+            services.AddControllersWithViews();
+
+            //services
+            //    .AddDefaultIdentity<User>(options =>
+            //options.Password.RequireDigit = false;
+            //options.Password.RequireLowercase = false;
+            //options.Password.RequireNonAlphanumeric = false;
+            //options.Password.RequireUppercase = false; })
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.PrepareDatabase();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+        }
+    }
+}
